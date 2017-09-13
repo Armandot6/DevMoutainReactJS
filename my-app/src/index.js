4eab1472f61438b0ee221271a2d49ab3ae87f430
Armando Tavares <armandotavares6@gmail.com>
2017-09-12 19:01:28 -0600

    jshint set

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 /* jshint esversion: 6 */
 
-=======
->>>>>>> cda52537fec73f350d1d3f9f1f6a632b23aca7b2
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
